d1bc0785d67a7c0d8a3faf8d2757eaf06700161a
Salman A <salmana.ar@gmail.com>
2024-02-12 23:15:39 +0530

    Update post router - Add getAll query - Remove unwanted routes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -8,33 +8,28 @@ import {
 import { posts } from "~/server/db/schema";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      await ctx.db.insert(posts).values({
-        name: input.name,
-        createdById: ctx.session.user.id,
-      });
-    }),
-
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.query.posts.findFirst({
       orderBy: (posts, { desc }) => [desc(posts.createdAt)],
     });
   }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.posts.findMany({
+      columns: { id: true, name: true, description: true, createdAt: true },
+      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    });
   }),
+
+  create: protectedProcedure
+    .input(
+      z.object({ name: z.string().min(1), description: z.string().min(1) }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(posts).values({
+        name: input.name,
+        description: input.description,
+        createdById: ctx.session.user.id,
+      });
+    }),
 });
